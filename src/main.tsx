ebc3ad4b69d00a65500ed2b4fbd4c3326ef233dd
Artur Kritsak <your@email.com>
2025-06-17 13:16:21 +0300

    revert: main.tsx back to original App component rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client'
-// import App from './App.tsx' // Temporarily removed for deep UI freeze diagnosis
+import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <div style={{ padding: '50px', textAlign: 'center', fontSize: '36px', color: 'blue' }}>
-    Якщо ви бачите це повідомлення, фронтенд працює!
-  </div>
-);
+createRoot(document.getElementById("root")!).render(<App />);
